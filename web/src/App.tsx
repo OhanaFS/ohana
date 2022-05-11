e9792201f905d7a159e05d98452d60553d47b32c
Sulabh Mehta <github@sulabh.ru>
2022-05-11 15:30:17 +0800

    Added frontend with app base

---
+++ web/src/App.tsx
@@ -0,0 +1,9 @@
+import AppBase from "./AppBase";
+
+export default function Demo() {
+  return (
+    <AppBase>
+      {/* Your application here */}
+    </AppBase>
+  );
+}
---
+++ web/src/MainLinks.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import { Home2, Star, Share } from 'tabler-icons-react';
+import { ThemeIcon, UnstyledButton, Group, Text } from '@mantine/core';
+
+interface MainLinkProps {
+  icon: React.ReactNode;
+  color: string;
+  label: string;
+}
+
+function MainLink({ icon, color, label }: MainLinkProps) {
+  return (
+    <UnstyledButton
+      sx={(theme) => ({
+        display: 'block',
+        width: '100%',
+        padding: theme.spacing.xs,
+        borderRadius: theme.radius.sm,
+        color: theme.colorScheme === 'dark' ? theme.colors.dark[0] : theme.black,
+
+        '&:hover': {
+          backgroundColor:
+            theme.colorScheme === 'dark' ? theme.colors.dark[6] : theme.colors.gray[0],
+        },
+      })}
+    >
+      <Group>
+        <ThemeIcon color={color} variant="light">
+          {icon}
+        </ThemeIcon>
+
+        <Text size="md">{label}</Text>
+      </Group>
+    </UnstyledButton>
+  );
+}
+
+const data = [
+  { icon: <Home2 size={16} />, color: 'blue', label: 'Home' },
+  { icon: <Star size={16} />, color: 'teal', label: 'Favourites' },
+  { icon: <Share size={16} />, color: 'violet', label: 'Shared' },
+];
+
+export function MainLinks() {
+  const links = data.map((link) => <MainLink {...link} key={link.label} />);
+  return <div>{links}</div>;
+}
