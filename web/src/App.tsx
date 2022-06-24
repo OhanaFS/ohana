f99fe060a33150f2b1ea02794f827b4907c5492c
Sulabh Mehta (Dominik) <64636803+sm32d@users.noreply.github.com>
2022-06-25 01:25:14 +0800

    modified some meta data

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,4 +1,5 @@
 //import AppBase from "./AppBase";
+import { Helmet } from 'react-helmet';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import AppBase from "./components/AppShell"; //switched to responsive base
 import { VFSBrowser } from './components/userFiles';
@@ -26,51 +27,30 @@ export default function Demo() {
 
 
   return (
-    
-
-  
-
-    
-       <Router>
+    <Router>
+      <Helmet>
+        <title>Ohana</title>
+      </Helmet>
       <Routes>
         <Route path='/files' element={<AppBase name='Alex' username='@alex' image='https://images.unsplash.com/photo-1496302662116-35cc4f36df92?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80'><></></AppBase>} />
         <Route path='/' element={<VFSBrowser />} />
-        <Route path='/admin' element={<LoginPage />}/>  
-        <Route path="/Admin_create_sso_key" element={<Admin_create_sso_key  />} />
-       <Route path="/Admin_ssogroups_inside" element={<Admin_ssogroups_inside/>} />
-       <Route path="/Admin_key_management" element={<Admin_key_management/>} />
-       <Route path="/Admin_maintenanceresults" element={<Admin_maintenanceresults/>} />
-       <Route path="/Admin_performmaintenance" element={<Admin_performmaintenance/>} />
-       <Route path="/Admin_maintenancesettings" element={<Admin_maintenancesettings/>} />
-       <Route path="/Admin_runmaintenance" element={<Admin_runmaintenance/>} />
-       <Route path="/Admin_statistics" element={<Admin_statistics />} />
-       <Route path="/Admin_ssogroups" element={<Admin_ssogroups/>} />
-       <Route path="/Admin_nodes" element={<Admin_nodes/>} />
-       <Route path="/Admin_maintenancelogs" element={<Admin_maintenancelogs/>} />
-       <Route path="/Admin_settings" element={<Admin_settings/>} />
-       <Route path="/Admin_configuration" element={<Admin_configuration/>} />
-       <Route path="/Admin_create_key" element={<Admin_create_key/>} />
-    
+        <Route path='/admin' element={<LoginPage />} />
+        <Route path="/Admin_create_sso_key" element={<Admin_create_sso_key />} />
+        <Route path="/Admin_ssogroups_inside" element={<Admin_ssogroups_inside />} />
+        <Route path="/Admin_key_management" element={<Admin_key_management />} />
+        <Route path="/Admin_maintenanceresults" element={<Admin_maintenanceresults />} />
+        <Route path="/Admin_performmaintenance" element={<Admin_performmaintenance />} />
+        <Route path="/Admin_maintenancesettings" element={<Admin_maintenancesettings />} />
+        <Route path="/Admin_runmaintenance" element={<Admin_runmaintenance />} />
+        <Route path="/Admin_statistics" element={<Admin_statistics />} />
+        <Route path="/Admin_ssogroups" element={<Admin_ssogroups />} />
+        <Route path="/Admin_nodes" element={<Admin_nodes />} />
+        <Route path="/Admin_maintenancelogs" element={<Admin_maintenancelogs />} />
+        <Route path="/Admin_settings" element={<Admin_settings />} />
+        <Route path="/Admin_configuration" element={<Admin_configuration />} />
+        <Route path="/Admin_create_key" element={<Admin_create_key />} />
+
       </Routes>
     </Router>
-
- 
-    
-    
-
-
-   
-
-
-
-    
-  
-
-
-
-
-
   );
-
-
 }
