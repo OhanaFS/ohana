e7ea4e55208c0e80b5f057a7ea50a69af72eda7b
Sulabh Mehta (Dominik) <64636803+sm32d@users.noreply.github.com>
2022-07-13 15:26:34 +0800

    Fixed save button to not go out of the screen at the bottom

--- web/src/AdminSettings.tsx
+++ web/src/AdminSettings.tsx
@@ -67,280 +67,281 @@ export function AdminSettings() {
   }
 
   return (
-   
-      <AppBase
-        userType="admin"
-        name="Alex Simmons"
-        username="@alex"
-        image="https://images.unsplash.com/photo-1496302662116-35cc4f36df92?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80"
+
+    <AppBase
+      userType="admin"
+      name="Alex Simmons"
+      username="@alex"
+      image="https://images.unsplash.com/photo-1496302662116-35cc4f36df92?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80"
+    >
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'flex-start',
+        }}
+
       >
-        <div
-          style={{
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'flex-start',
-          }}
+        <div className="settings">
+          <Table striped verticalSpacing="md"     >
+            <caption
+              style={{ fontWeight: '600', fontSize: '22px', color: 'black' }}
+            >
+              <span style={{ textAlign: 'center' }}>
+                Notification Settings
+              </span>
+            </caption>
 
-        >
-          <div className="settings">
-            <Table striped verticalSpacing="md"     >
-              <caption
-                style={{ fontWeight: '600', fontSize: '22px', color: 'black' }}
-              >
-                <span style={{ textAlign: 'center' }}>
-                  Notification Settings
-                </span>
-              </caption>
+            <thead></thead>
+            <tbody style={{}}>
+              <tr>
+                <td
 
-              <thead></thead>
-              <tbody style={{}}>
-                <tr>
-                  <td
+                  style={{
+                    textAlign: 'left',
+                    fontWeight: '400',
+                    fontSize: '18px',
+                    color: 'black',
+                  }}
+                >
 
-                    style={{
-                      textAlign: 'left',
-                      fontWeight: '400',
-                      fontSize: '18px',
-                      color: 'black',
-                    }}
-                  >
-
-                    <Text style={{}}> Allow Cluster health alerts </Text>
-                  </td>
-                  <td style={{ position: 'relative' }}>
-                    <Checkbox
-                      size="md"
-                      style={{
-                        position: 'absolute',
-                        top: '18px',
-                        right: '16px'
-                      }}
-                      checked={clusterAlerts}
-                      onChange={(event) => [
-                        setChecked(event.currentTarget.checked),
-                        setDisable(event.currentTarget.checked),
-                      ]}
-                    />{' '}
-                    {' '}
-                  </td>
-                </tr>
-
-                <tr>
-                  <td
-
-                    style={{
-                      textAlign: 'left',
-                      fontWeight: '400',
-                      fontSize: '18px',
-                      color: 'black',
-                    }}
-                  >
-                    {' '}
-                    <Text style={{}}> Allow server offline alerts </Text>
-                  </td>
-                  <td style={{ position: 'relative' }}>
-
-                    <Checkbox
-                      size="md"
-                      id="1"
-                      style={{
-                        position: 'absolute',
-                        top: '18px',
-                        right: '16px'
-                      }}
-                      checked={sActionAlerts}
-                      onChange={(event) => [
-                        setChecked1(event.currentTarget.checked),
-                        setDisable(event.currentTarget.checked),
-                      ]}
-                    />
-
-                  </td>
-                </tr>
-
-                <tr>
-                  <td
-
-                    style={{
-                      textAlign: 'left',
-                      fontWeight: '400',
-                      fontSize: '18px',
-                      color: 'black',
-                    }}
-                  >
-                    {' '}
-                    <Text style={{}}> Allow supicious action alerts </Text>{' '}
-                  </td>
-                  <td style={{ position: 'relative' }}>
-
-                    <Checkbox
-                      size="md"
-                      id="1"
-                      style={{
-                        position: 'absolute',
-                        top: '18px',
-                        right: '16px'
-                      }}
-                      checked={supiciousAlerts}
-                      onChange={(event) => [
-                        setChecked2(event.currentTarget.checked),
-                        setDisable(event.currentTarget.checked),
-                      ]}
-                    />{' '}
-
-                  </td>
-                </tr>
-
-                <tr>
-                  <td
-
-                    style={{
-                      textAlign: 'left',
-                      fontWeight: '400',
-                      fontSize: '18px',
-                      color: 'black',
-                    }}
-                  >
-                    {' '}
-                    <span style={{}}> Allow server full alert </span>{' '}
-                  </td>
-                  <td style={{ position: 'relative' }} >
-
-                    <Checkbox
-                      size="md"
-                      id="1"
-                      style={{
-                        position: 'absolute',
-                        top: '18px',
-                        right: '16px'
-                      }}
-                      checked={serverAlerts}
-                      onChange={(event) => [
-                        setChecked3(event.currentTarget.checked),
-                        setDisable(event.currentTarget.checked),
-                      ]}
-                    />{' '}
-
-                  </td>
-                </tr>
-
-                <tr>
-                  <td
-
-                    style={{
-                      textAlign: 'left',
-                      fontWeight: '400',
-                      fontSize: '18px',
-                      color: 'black',
-                    }}
-                  >
-                    {' '}
-                    <span style={{}}> Allow supicious file alerts </span>
-                  </td>
-
-                  <td style={{ position: 'relative' }}>
-
-                    {' '}
-                    <Checkbox
-                      size="md"
-                      id="1"
-                      style={{
-                        position: 'absolute',
-                        top: '18px',
-                        right: '16px'
-                      }}
-                      checked={sFileAlerts}
-                      onChange={(event) => [
-                        setChecked4(event.currentTarget.checked),
-                        setDisable(event.currentTarget.checked),
-                      ]}
-                    />
-
-                  </td>
-
-                </tr>
-
-                <tr>
-                  <td
-                    style={{
-                      textAlign: 'left',
-                      fontWeight: '400',
-                      fontSize: '18px',
-                      color: 'black',
-                    }}
-                  >
-                    {' '}
-                    Backup encryption key{' '}
-                    <Text weight={700}>
-                      Current Location:{currentLocation}{' '}
-                    </Text>{' '}
-                  </td>
-                  <td>
-                    {' '}
-                    <Button
-                      style={{ float: 'right' }}
-                      variant="default"
-                      color="dark"
-                      size="md"
-                    >
-                      {' '}
-                      Backup
-                    </Button>
-                  </td>
-                </tr>
-
-                <tr>
-                  <td
-                    style={{
-                      textAlign: 'left',
-                      fontWeight: '400',
-                      fontSize: '18px',
-                      color: 'black',
-                    }}
-                  >
-                    {' '}
-                    Change the redundancy level of the files{' '}
-                    <Text weight={700}>
-                      Current redundancy level:{redundancy}{' '}
-                    </Text>{' '}
-                  </td>
-                  <td>
-                    {' '}
-                    <Button
-                      style={{ float: 'right' }}
-                      variant="default"
-                      color="dark"
-                      size="md"
-                    >
-                      {' '}
-                      Change
-                    </Button>
-                  </td>
-                </tr>
-              </tbody>
-              <tfoot>
-
-                <td colSpan={2} style={{ position: 'relative' }}>
-                  <Button
-                    disabled={disable}
+                  <Text style={{}}> Allow Cluster health alerts </Text>
+                </td>
+                <td style={{ position: 'relative' }}>
+                  <Checkbox
+                    size="md"
                     style={{
                       position: 'absolute',
                       top: '18px',
                       right: '16px'
                     }}
+                    checked={clusterAlerts}
+                    onChange={(event) => [
+                      setChecked(event.currentTarget.checked),
+                      setDisable(event.currentTarget.checked),
+                    ]}
+                  />{' '}
+                  {' '}
+                </td>
+              </tr>
+
+              <tr>
+                <td
+
+                  style={{
+                    textAlign: 'left',
+                    fontWeight: '400',
+                    fontSize: '18px',
+                    color: 'black',
+                  }}
+                >
+                  {' '}
+                  <Text style={{}}> Allow server offline alerts </Text>
+                </td>
+                <td style={{ position: 'relative' }}>
+
+                  <Checkbox
+                    size="md"
+                    id="1"
+                    style={{
+                      position: 'absolute',
+                      top: '18px',
+                      right: '16px'
+                    }}
+                    checked={sActionAlerts}
+                    onChange={(event) => [
+                      setChecked1(event.currentTarget.checked),
+                      setDisable(event.currentTarget.checked),
+                    ]}
+                  />
+
+                </td>
+              </tr>
+
+              <tr>
+                <td
+
+                  style={{
+                    textAlign: 'left',
+                    fontWeight: '400',
+                    fontSize: '18px',
+                    color: 'black',
+                  }}
+                >
+                  {' '}
+                  <Text style={{}}> Allow supicious action alerts </Text>{' '}
+                </td>
+                <td style={{ position: 'relative' }}>
+
+                  <Checkbox
+                    size="md"
+                    id="1"
+                    style={{
+                      position: 'absolute',
+                      top: '18px',
+                      right: '16px'
+                    }}
+                    checked={supiciousAlerts}
+                    onChange={(event) => [
+                      setChecked2(event.currentTarget.checked),
+                      setDisable(event.currentTarget.checked),
+                    ]}
+                  />{' '}
+
+                </td>
+              </tr>
+
+              <tr>
+                <td
+
+                  style={{
+                    textAlign: 'left',
+                    fontWeight: '400',
+                    fontSize: '18px',
+                    color: 'black',
+                  }}
+                >
+                  {' '}
+                  <span style={{}}> Allow server full alert </span>{' '}
+                </td>
+                <td style={{ position: 'relative' }} >
+
+                  <Checkbox
+                    size="md"
+                    id="1"
+                    style={{
+                      position: 'absolute',
+                      top: '18px',
+                      right: '16px'
+                    }}
+                    checked={serverAlerts}
+                    onChange={(event) => [
+                      setChecked3(event.currentTarget.checked),
+                      setDisable(event.currentTarget.checked),
+                    ]}
+                  />{' '}
+
+                </td>
+              </tr>
+
+              <tr>
+                <td
+
+                  style={{
+                    textAlign: 'left',
+                    fontWeight: '400',
+                    fontSize: '18px',
+                    color: 'black',
+                  }}
+                >
+                  {' '}
+                  <span style={{}}> Allow supicious file alerts </span>
+                </td>
+
+                <td style={{ position: 'relative' }}>
+
+                  {' '}
+                  <Checkbox
+                    size="md"
+                    id="1"
+                    style={{
+                      position: 'absolute',
+                      top: '18px',
+                      right: '16px'
+                    }}
+                    checked={sFileAlerts}
+                    onChange={(event) => [
+                      setChecked4(event.currentTarget.checked),
+                      setDisable(event.currentTarget.checked),
+                    ]}
+                  />
+
+                </td>
+
+              </tr>
+
+              <tr>
+                <td
+                  style={{
+                    textAlign: 'left',
+                    fontWeight: '400',
+                    fontSize: '18px',
+                    color: 'black',
+                  }}
+                >
+                  {' '}
+                  Backup encryption key{' '}
+                  <Text weight={700}>
+                    Current Location:{currentLocation}{' '}
+                  </Text>{' '}
+                </td>
+                <td>
+                  {' '}
+                  <Button
+                    style={{ float: 'right' }}
                     variant="default"
                     color="dark"
                     size="md"
                   >
-                    Save Pending Changes
+                    {' '}
+                    Backup
                   </Button>
                 </td>
-              </tfoot>
-            </Table>
-          </div>
+              </tr>
+
+              <tr>
+                <td
+                  style={{
+                    textAlign: 'left',
+                    fontWeight: '400',
+                    fontSize: '18px',
+                    color: 'black',
+                  }}
+                >
+                  {' '}
+                  Change the redundancy level of the files{' '}
+                  <Text weight={700}>
+                    Current redundancy level:{redundancy}{' '}
+                  </Text>{' '}
+                </td>
+                <td>
+                  {' '}
+                  <Button
+                    style={{ float: 'right' }}
+                    variant="default"
+                    color="dark"
+                    size="md"
+                  >
+                    {' '}
+                    Change
+                  </Button>
+                </td>
+              </tr>
+            </tbody>
+            <tfoot>
+
+              <td colSpan={2} style={{ position: 'relative' }}>
+                <Button
+                  disabled={disable}
+                  style={{
+                    position: 'absolute',
+                    top: '18px',
+                    right: '16px',
+                  }}
+                  variant="default"
+                  color="dark"
+                  size="md"
+                >
+                  Save Pending Changes
+                </Button>
+                <div style={{ marginBottom: '80px' }} />
+              </td>
+            </tfoot>
+          </Table>
         </div>
-      </AppBase>
-   
+      </div>
+    </AppBase>
+
   );
 }
 
