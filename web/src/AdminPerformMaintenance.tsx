dac89f972560c8ec2aca8f3b5172f03985eb75db
voidxpride@gmail.com <asd@gmail.com>
2022-07-21 19:29:02 +0800

    fix the timer for maintenance and added the maintenance logs

--- web/src/AdminPerformMaintenance.tsx
+++ web/src/AdminPerformMaintenance.tsx
@@ -7,20 +7,58 @@ import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 export function AdminPerformMaintenance() {
 
+  const steps = [
+  'Cleaning Server 1',
+  'Finish Cleaning Server 1' ,
+  'Added Server 2 ', 
+  'Finish Cleaning Server 2',
+  'Added Server 3 ', 
+  'Finish Cleaning Server 3',
+  'Added Server 4 ', 
+  'Finish Cleaning Server 4',
+  'Added Server 5 ', 
+  'Finish Cleaning Server 5',
+  'Added Server 6 ', 
+  'Finish Cleaning Server 6',
+  'Added Server 7 ', 
+  'Finish Cleaning Server 7',
+  ];
+
+  const [elapsedTime,setElapsedTime] = useState(0);
+ 
   const timerProps = {
     isPlaying: true,
     size: 300,
     strokeWidth: 10
   };
+
+  //get the maintenance of time needed based on how much steps
+  const time = steps.length;
+  const minuteSeconds = time;
+  const getTimeSeconds = (time:number) => (minuteSeconds - time) | 0;
+
+  function secondsToDhms(seconds: number) {
+    seconds = Number(seconds);
+    var d = Math.floor(seconds / (3600 * 24));
+    var h = Math.floor(seconds % (3600 * 24) / 3600);
+    var m = Math.floor(seconds % 3600 / 60);
+    var s = Math.floor(seconds % 60);
+    var dDisplay = d > 0 ? d + (d == 1 ? " day, " : " days, ") : "";
+    var hDisplay = h > 0 ? h + (h == 1 ? " hour, " : " hrs, ") : "";
+    var mDisplay = m > 0 ? m + (m == 1 ? " minute, " : " mins, ") : "";
+    var sDisplay = s > 0 ? s + (s == 1 ? " second" : " secs") : "";
+    return dDisplay + hDisplay + mDisplay + sDisplay;
+  }
   
+// convert the number of seconds into day hour month and seconds
   const renderTime = (dimension:string, time:number) => {
     return (
       <div >
-        <div >{time}</div>
-        <div>{dimension}</div>
+        <div>{secondsToDhms(time)}</div>
       </div>
     );
   };
+
 
   const [isActive, setIsActive] = useState(true);
   const [buttonText, setButtonText] = useState('Pause');
@@ -37,33 +75,10 @@ export function AdminPerformMaintenance() {
     
   }
   function stop(){
-
     setIsActive(!isActive);
     navigate('/maintenanceresults');
   }
 
-
-
-  
-
-  const minuteSeconds = 60;
-  const hourSeconds = 3600;
-  const daySeconds = 86400;
-  
-  const getTimeSeconds = (time:number) => (minuteSeconds - time) | 0;
-  const getTimeMinutes = (time:number) => ((time % hourSeconds) / minuteSeconds) | 0;
-  const getTimeHours = (time:number) => ((time % daySeconds) / hourSeconds) | 0;
-  const getTimeDays = (time:number) => (time / daySeconds) | 0;
- 
-
-
-  const stratTime = Date.now() / 1000; // use UNIX timestamp in seconds
-
-  //this is where the total time 
-  const endTime = stratTime + 9020; // use UNIX timestamp in seconds
-  const remainingTime = endTime - stratTime;
-  const days = Math.ceil(remainingTime / daySeconds);
-  
   return (
     <>  
 
@@ -94,30 +109,43 @@ export function AdminPerformMaintenance() {
           backgroundColor:'white'
           
         }}>
+       
           <div style={{
               display: 'flex',
               justifyContent: 'center',
               flexDirection: 'row',
+              marginTop:'20px'
+              
+            }}> 
+            <span style={{
+                fontWeight:500,
+                fontSize:'22px'
+            }}>
+              Maintenance in Progress
+            </span>
             
-            }}><h3>Maintenance in Progress</h3></div>
+            </div>
           <div
             style={{
               display: 'flex',
               justifyContent: 'center',
               flexDirection: 'row',
-            
+              marginTop:'20px'
             }}>
       <CountdownCircleTimer
         {...timerProps}
       
         colors="#218380"
-        duration={8}
+        duration={time}
         isPlaying={isActive}
-        initialRemainingTime={8}
+        initialRemainingTime={time}
         onComplete={() => stop()}
       >
         {({ elapsedTime, color }) => (
           <>
+            {setElapsedTime(Math.floor(elapsedTime))}
+     
+        
           <div style={{ color, display:'flex',flexDirection: 'column',textAlign:'center'
          }}>
            <div style={{color, display:'flex',flexDirection: 'column'}}>
@@ -125,20 +153,19 @@ export function AdminPerformMaintenance() {
           </div>
 
           <div style={{ color, display:'flex',flexDirection: 'row',marginTop:'20px'
-         }}><div>
-            {renderTime("hours", getTimeHours(daySeconds - elapsedTime))}
-            </div>
-            <div style={{marginLeft:'20px'}}>
-            {renderTime("minutes", getTimeMinutes(hourSeconds - elapsedTime))}
-            </div>
+         }}>
             <div style={{marginLeft:'20px'}} >
             {renderTime("seconds", getTimeSeconds(elapsedTime))}
             
             </div>
+          
           </div>
+          
          <div style={{marginTop:'20px'}}>
-         % Completed: {elapsedTime/8 *100 | 0 }
+          
+         % Completed: {elapsedTime/(time) *100 | 0 }
          </div>
+   
           </div>
           </>
         )}
@@ -146,8 +173,19 @@ export function AdminPerformMaintenance() {
       </div>
       <div style={{
               display: 'flex',
+              justifyContent: 'center',
               flexDirection: 'row',
+              marginTop:'20px',
+              marginBottom:'20px'
+            }}>
+
+            Maintenance Logs : {steps[Math.floor(elapsedTime)]}
+          
+      </div>
+      <div style={{
+              display: 'flex',
               justifyContent: 'space-between',
+              flexDirection: 'row',
               marginTop:'20px',
               marginBottom:'20px'
             }}>
