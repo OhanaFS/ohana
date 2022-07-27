3bc544eec78ec091f5660febeb11c368acb4ef26
Hizkia Felix <felix@hizkifw.me>
2022-07-27 14:14:18 +0800

    wip: react query (#36)

--- web/src/api/api.ts
+++ web/src/api/api.ts
@@ -4,3 +4,22 @@ export const APIClient = axios.create({
   baseURL: '/',
   withCredentials: true,
 });
+
+export type ErrorResponse = {
+  code: string;
+  message: string;
+};
+
+/**
+ * typedError re-throws an error as the ErrorResponse type.
+ */
+export const typedError = (err: any) => {
+  throw axios.isAxiosError(err) && err.response
+    ? (err.response.data as ErrorResponse)
+    : { code: '-1', message: err.toString() };
+};
+
+export const qs = (obj: any) =>
+  Object.keys(obj)
+    .map((key) => [key, obj[key]].map(encodeURIComponent).join('='))
+    .join('&');
