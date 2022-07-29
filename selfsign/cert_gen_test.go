ce595b98a23523161ccc60df76ffea2fed775456
Adriel Tan <23271545+adrieltan1@users.noreply.github.com>
2022-07-30 00:35:51 +0800

    fix cert_gen_test by changing port#
    >> It runs fine on my system
    
    Not sure if docker is doing some concurrency thing that causes it to fail and not deliver properly.

--- selfsign/cert_gen_test.go
+++ selfsign/cert_gen_test.go
@@ -138,7 +138,7 @@ func TestSelfSign(t *testing.T) {
 			ClientAuth: tls.RequireAndVerifyClientCert,
 		}
 
-		srv := NewServer("5555", tlsConfig)
+		srv := NewServer("5556", tlsConfig)
 		go srv.ListenAndServeTLS(*ogc.GenCertsPath+"_cert.pem", *ogc.GenCertsPath+"_key.pem")
 		defer srv.Close()
 
@@ -159,7 +159,7 @@ func TestSelfSign(t *testing.T) {
 			},
 		}
 
-		res, err := client.Get("https://localhost:5555")
+		res, err := client.Get("https://localhost:5556")
 		Assert.NoError(err)
 
 		defer res.Body.Close()
@@ -191,7 +191,7 @@ func TestSelfSign(t *testing.T) {
 			},
 		}
 
-		res, err = client2.Get("https://localhost:5555")
+		res, err = client2.Get("https://localhost:5556")
 		Assert.IsType(err, &url.Error{}) // should be  x509.UnknownAuthorityError{}
 
 	})
