4cb58fc81df8816ba07002a60df84b73d31acad5
voidxpride@gmail.com <asd@gmail.com>
2022-08-04 16:23:21 +0800

    able to assign permissions Read , Write , and Execute to API keys

--- web/src/AdminKeyManagement.tsx
+++ web/src/AdminKeyManagement.tsx
@@ -1,26 +1,531 @@
+import {
+  Button,
+  Modal,
+  Textarea,
+  ScrollArea,
+  Text,
+  Table,
+  Checkbox,
+  Radio,
+} from '@mantine/core';
 import { useState } from 'react';
-import { AdminConsole } from './AdminConsole';
+import { Link } from 'react-router-dom';
 import AppBase from './components/AppBase';
 
+export interface Key {
+  key: string;
+  permissions: Array<any>;
+  type: string;
+  location: string;
+}
 export function AdminKeyManagement() {
-  // data to be pass in
-  let [data, setValue] = useState([
-    '128c1d5d-2359-4ba1-8739-2cd30d694d67',
-    '128c1d5d-2359-4ba1-8739-2cd30d69sds67',
-  ]);
+  const data = [
+    {
+      id: '128c1d5d-2359-4ba1-8739-2cd30d694d67',
+      permissions: ['Read', 'Write', 'Execute'],
+      type: 'path',
+      location: 'asd',
+    },
+
+    {
+      id: '128c1eee-2359-4ba1-8739-2cd30d69sds67',
+      permissions: ['Read', 'Write'],
+      type: 'filename prefix',
+      location: 'asd',
+    },
+  ];
+  // Variable that show all the ApiKey
+  const [ApiKey, setValue] = useState(data);
+
+  // Variable that will be added to the CurrentSSOGroups
+  var [key, addKey] = useState('');
+  var [permissions, addPermissions] = useState<string[]>([]);
+  var [type, addType] = useState('');
+  var [location, addLocation] = useState('');
+  var [index, addIndex] = useState(Number);
+
+  // Variable that will decide whether the submit button is disabled
+  var [submitBtn, setSubmitBtn] = useState(true);
+
+  // Variable that will decide whether the addUserModel visibility is true or false
+  const [openedAddUserModel, setOpened] = useState(false);
+  const [openedUpdateUserModel, setOpened2] = useState(false);
+
+  // Variable that is bind to each specific labels
+  const title = 'Add Key';
+  const textField = 'Name of the Key';
+
+  // Variable that will decide the value of the errorMessage that will be displayed
+  var [errorMessage, setErrorMessage] = useState('');
+  var [errorMessage2, setErrorMessage2] = useState('');
+  var [errorMessage3, setErrorMessage3] = useState('');
+  var [errorMessage4, setErrorMessage4] = useState('');
+
+  // Delete away specific group from CurrentSSOGroups
+  const deleteGroup = (index: any) => {
+    setValue(ApiKey.filter((v, i) => i !== index));
+  };
+  const updateGroup = (index: any) => {
+    addKey(ApiKey[index].id);
+    addPermissions(ApiKey[index].permissions);
+    addType(ApiKey[index].type);
+    addLocation(ApiKey[index].location);
+    addIndex(index);
+    setOpened2(true);
+  };
+  // Add the group to the CurrentSSOGroups and set the addUserModel visibility to false
+  function add() {
+    const vkey = [
+      {
+        id: key,
+        permissions: permissions,
+        type: type,
+        location: location,
+      },
+    ];
+    setValue([...ApiKey, ...vkey]);
+    setOpened(false);
+  }
+
+  function update() {
+    const vkey = [
+      {
+        id: key,
+        permissions: permissions,
+        type: type,
+        location: location,
+      },
+    ];
+    ApiKey[index].id = vkey[0].id;
+    ApiKey[index].permissions = vkey[0].permissions;
+    ApiKey[index].type = vkey[0].type;
+    ApiKey[index].location = vkey[0].location;
+    setOpened2(false);
+  }
+  /* Validate the textfield to check if there is any special characters
+     if there is special character, the function will display error message 
+     and set the submit button to false.  */
+  function validate() {
+    if (
+      key.includes('/') ||
+      key.includes('[') ||
+      key.includes('!') ||
+      key.includes('@') ||
+      key.includes('#') ||
+      key.includes('$') ||
+      key.includes('%') ||
+      key.includes('^') ||
+      key.includes('&') ||
+      key.includes('*') ||
+      key.includes('(') ||
+      key.includes(')') ||
+      key.includes('\\') ||
+      key.includes('=') ||
+      key.includes('[') ||
+      key.includes(']') ||
+      key.includes(';') ||
+      key.includes(',') ||
+      key.includes('.') ||
+      key.includes('<') ||
+      key.includes('>') ||
+      key.includes('?') ||
+      key.includes('`')
+    ) {
+      errorMessage = 'do not include special characters';
+      setErrorMessage('do not include special characters');
+      submitBtn = true;
+      setSubmitBtn(true);
+    } else if (key.includes(' ')) {
+      errorMessage = 'No space is allowed';
+      setErrorMessage('No space is allowed');
+      submitBtn = true;
+      setSubmitBtn(true);
+    } else if (key == '') {
+      errorMessage = 'Details needed';
+      setErrorMessage('Details needed');
+      submitBtn = true;
+      setSubmitBtn(true);
+    } else {
+      errorMessage = '';
+      setErrorMessage('');
+    }
+    if (
+      location.includes('/') ||
+      location.includes('[') ||
+      location.includes('!') ||
+      location.includes('@') ||
+      location.includes('#') ||
+      location.includes('$') ||
+      location.includes('%') ||
+      location.includes('^') ||
+      location.includes('&') ||
+      location.includes('*') ||
+      location.includes('(') ||
+      location.includes(')') ||
+      location.includes('\\') ||
+      location.includes('=') ||
+      location.includes('[') ||
+      location.includes(']') ||
+      location.includes(';') ||
+      location.includes(',') ||
+      location.includes('.') ||
+      location.includes('<') ||
+      location.includes('>') ||
+      location.includes('?') ||
+      location.includes('`')
+    ) {
+      errorMessage2 = 'do not include special characters';
+      setErrorMessage2('do not include special characters');
+      submitBtn = true;
+      setSubmitBtn(true);
+    } else if (location.includes(' ')) {
+      errorMessage2 = 'No space is allowed';
+      setErrorMessage2('No space is allowed');
+      submitBtn = true;
+      setSubmitBtn(true);
+    } else if (location == '') {
+      errorMessage2 = 'Details needed';
+      setErrorMessage2('Details needed');
+      submitBtn = true;
+      setSubmitBtn(true);
+    } else {
+      errorMessage2 = '';
+      setErrorMessage2('');
+    }
+    if (type == '') {
+      errorMessage3 = 'Choose the type';
+      setErrorMessage3('Choose the type');
+    } else {
+      errorMessage3 = '';
+      setErrorMessage3('');
+    }
+    if (permissions.length === 0) {
+      errorMessage4 = 'Choose the permissions';
+      setErrorMessage4('Choose the permissions');
+    } else {
+      errorMessage4 = '';
+      setErrorMessage4('');
+    }
+
+    if (
+      errorMessage == '' &&
+      errorMessage2 == '' &&
+      errorMessage3 == '' &&
+      errorMessage4 == ''
+    ) {
+      submitBtn = false;
+      setSubmitBtn(false);
+    }
+  }
+
+  // display table header that is from props
+  const ths = (
+    <tr>
+      <th
+        style={{
+          textAlign: 'left',
+          fontWeight: '700',
+          fontSize: '16px',
+          color: 'black',
+        }}
+      >
+        <div style={{ marginLeft: '10px' }}>Key ID</div>
+      </th>
+    </tr>
+  );
+
+  // display all the rows that is from props
+  const rows = ApiKey.map((items, index) => (
+    <tr key={index}>
+      <td
+        style={{
+          display: 'flex',
+          justifyContent: 'space-between',
+        }}
+      >
+        <Text
+          color="dark"
+          style={{
+            marginLeft: '10px',
+            pointerEvents: false ? 'auto' : 'none',
+          }}
+          component={Link}
+          to="/insidessogroup"
+          variant="link"
+        >
+          {items['id']}
+        </Text>
+        <div>
+        <Button
+          variant="default"
+          color="dark"
+          size="md"
+          style={{   marginRight:"10px"}}
+          onClick={() => [updateGroup(index)]}
+        >
+          Update
+        </Button>
+        <Button
+          variant="default"
+          color="dark"
+          size="md"
+          style={{}}
+          onClick={() => [deleteGroup(index)]}
+        >
+          Delete
+        </Button>
+        </div>
+      </td>
+    </tr>
+  ));
 
   return (
     <>
       <AppBase userType="admin">
-        <AdminConsole
-          groupList={data}
-          addObjectLabel="Key"
-          deleteObjectLabel="Key"
-          tableHeader={['Key ID']}
-          tableBody={[]}
-          caption="API Key Management Console"
-          pointerEvents={false}
-        ></AdminConsole>
+        <div
+          style={{
+            display: 'flex',
+            height: '50vh',
+            justifyContent: 'center',
+          }}
+        >
+          <div className="console">
+            <Modal
+              centered
+              title="Add console"
+              opened={openedAddUserModel}
+              onClose={() => setOpened(false)}
+              size="20%"
+            >
+              {
+                <div
+                  style={{
+                    display: 'flex',
+                    height: '50vh',
+                    flexDirection: 'column',
+                  }}
+                >
+                  <Textarea
+                    placeholder={textField}
+                    label={title}
+                    defaultValue={key}
+                    size="md"
+                    name="password"
+                    id="password"
+                    required
+                    error={errorMessage}
+                    onChange={(event) => {
+                      addKey(event.target.value),
+                        (key = event.target.value),
+                        validate();
+                    }}
+                  />
+                  <Radio.Group
+                    orientation="vertical"
+                    label={
+                      <span style={{ fontSize: '16px' }}>
+                        Choose type of key
+                      </span>
+                    }
+                    error={errorMessage3}
+                    spacing="sm"
+                    required
+                    value={type}
+                    onChange={(value) => (
+                      addType(value), (type = value), validate()
+                    )}
+                  >
+                    <Radio value="filenameprefix" label="Filename Prefix" />
+                    <Radio value="path" label="Path" />
+                  </Radio.Group>
+                  <Checkbox.Group
+                    label={
+                      <span style={{ fontSize: '16px' }}>
+                        Choose the Permissions
+                      </span>
+                    }
+                    spacing="xl"
+                    required
+                    value={permissions}
+                    error={errorMessage4}
+                    onChange={(value) => {
+                      addPermissions(value), (permissions = value), validate();
+                    }}
+                  >
+                    <Checkbox value="Read" label="Read" />
+                    <Checkbox value="Write" label="Write" />
+                    <Checkbox value="Execute" label="Execute" />
+                    <Checkbox value="Share" label="Share" />
+                    <Checkbox value="Audit" label="Audit" />
+                  </Checkbox.Group>
+
+                  <Textarea
+                    label="Location:"
+                    radius="md"
+                    size="lg"
+                    error={errorMessage2}
+                    defaultValue={location}
+                    required
+                    onChange={(event) => [
+                      addLocation(event.target.value),
+                      (location = event.target.value),
+                      validate(),
+                    ]}
+                  />
+                  <Button
+                    variant="default"
+                    color="dark"
+                    size="md"
+                    disabled={submitBtn}
+                    onClick={() => add()}
+                    style={{
+                      marginLeft: '15px',
+                      alignSelf: 'flex-end',
+                      marginTop: '20px',
+                    }}
+                  >
+                    Submit
+                  </Button>
+                </div>
+              }
+            </Modal>
+            <Modal
+              centered
+              title="Update console"
+              opened={openedUpdateUserModel}
+              onClose={() => setOpened2(false)}
+              size="20%"
+            >
+              {
+                <div
+                  style={{
+                    display: 'flex',
+                    height: '45vh',
+                    flexDirection: 'column',
+                  }}
+                >
+                  <Textarea
+                    placeholder={textField}
+                    label="Key"
+                    defaultValue={key}
+                    size="md"
+                    name="password"
+                    id="password"
+                    required
+                    error={errorMessage}
+                    onChange={(event) => {
+                      addKey(event.target.value),
+                        (key = event.target.value),
+                        validate();
+                    }}
+                  />
+                  <Radio.Group
+                    orientation="vertical"
+                    label={
+                      <span style={{ fontSize: '16px' }}>Type of key</span>
+                    }
+                    error={errorMessage3}
+                    spacing="sm"
+                    required
+                    value={type}
+                    onChange={(value) => (
+                      addType(value), (type = value), validate()
+                    )}
+                  >
+                    <Radio value="filename prefix" label="filename prefix" />
+                    <Radio value="path" label="path" />
+                  </Radio.Group>
+                  <Checkbox.Group
+                    label={
+                      <span style={{ fontSize: '16px' }}>Permissions</span>
+                    }
+                    spacing="xl"
+                    required
+                    value={permissions}
+                    error={errorMessage4}
+                    onChange={(value) => {
+                      addPermissions(value), (permissions = value), validate();
+                    }}
+                  >
+                    <Checkbox value="Read" label="Read" />
+                    <Checkbox value="Write" label="Write" />
+                    <Checkbox value="Execute" label="Execute" />
+                    <Checkbox value="Share" label="Share" />
+                    <Checkbox value="Audit" label="Audit" />
+                  </Checkbox.Group>
+
+                  <Textarea
+                    label="Location:"
+                    radius="md"
+                    size="lg"
+                    error={errorMessage2}
+                    defaultValue={location}
+                    required
+                    onChange={(event) => [
+                      addLocation(event.target.value),
+                      (location = event.target.value),
+                      validate(),
+                    ]}
+                  />
+                  <Button
+                    variant="default"
+                    color="dark"
+                    size="md"
+                    onClick={() => update()}
+                    style={{
+                      marginLeft: '15px',
+                      alignSelf: 'flex-end',
+                      marginTop: '20px',
+                    }}
+                  >
+                    Update
+                  </Button>
+                </div>
+              }
+            </Modal>
+            <ScrollArea
+              style={{
+                height: '90%',
+                width: '100%',
+                marginTop: '1%',
+              }}
+            >
+              <Table captionSide="top" verticalSpacing="sm">
+                <caption
+                  style={{
+                    textAlign: 'center',
+                    fontWeight: '600',
+                    fontSize: '24px',
+                    color: 'black',
+                    marginTop: '5px',
+                  }}
+                >
+                  {'API Key Management Console'}{' '}
+                </caption>
+                <thead style={{}}>{ths}</thead>
+                <tbody>{rows}</tbody>
+              </Table>
+            </ScrollArea>
+
+            <div
+              style={{
+                display: 'flex',
+                flexDirection: 'row',
+                justifyContent: 'space-between',
+              }}
+            >
+              <Button
+                variant="default"
+                color="dark"
+                size="md"
+                onClick={() => setOpened(true)}
+                style={{ marginLeft: '15px' }}
+              >
+                Add Key
+              </Button>
+            </div>
+          </div>
+        </div>
       </AppBase>
     </>
   );
--- web/src/AdminSsoGroupsInside.tsx
+++ web/src/AdminSsoGroupsInside.tsx
@@ -1,21 +1,257 @@
+import { Modal, Textarea, Button, ScrollArea,Table,Text } from '@mantine/core';
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+
 import { AdminConsole } from './AdminConsole';
 import AppBase from './components/AppBase';
 
 export function AdminSsoGroupsInside() {
-  const data: Array<any> = ['Tom', 'Peter', 'Raymond'];
+  const data: Array<any> = [
 
+  {
+    name: 'Tom',
+    limit: '30'
+  },
+  {
+    name: 'Mary',
+    limit: '30'
+  },
+  {
+    name: 'Peter',
+    limit: '30'
+  },
+  ]
+ // Variable that show all the currentSSOGroups inside the props.groupList
+ const [CurrentSSOGroups, setValue] = useState(data);
+
+ // Variable that will be added to the CurrentSSOGroups
+ var [Group, addGroup] = useState('');
+
+ // Variable that will decide whether the submit button is disabled
+ var [submitBtn, setSubmitBtn] = useState(true);
+
+ // Variable that will decide whether the addUserModel visibility is true or false
+ const [openedAddUserModel, setOpened] = useState(false);
+
+ // Variable that is bind to each specific labels
+ const title = 'Add User ';
+ const textField = 'Name of the User';
+
+ // Variable that will decide whether the errorMessage will be displayed
+ var [errorMessage, setErrorMessage] = useState('');
+
+ // Delete away specific group from CurrentSSOGroups
+ const deleteGroup = (index: any) => {
+   setValue(CurrentSSOGroups.filter((v, i) => i !== index));
+ };
+
+ // Add the group to the CurrentSSOGroups and set the addUserModel visibility to false
+ function add() {
+   setValue(CurrentSSOGroups.concat(Group));
+   setOpened(false);
+ }
+
+ /* Validate the textfield to check if there is any special characters
+    if there is special character, the function will display error message 
+    and set the submit button to false.  */
+ function validate() {
+   if (
+     Group.includes('/') ||
+     Group.includes('[') ||
+     Group.includes('!') ||
+     Group.includes('@') ||
+     Group.includes('#') ||
+     Group.includes('$') ||
+     Group.includes('%') ||
+     Group.includes('^') ||
+     Group.includes('&') ||
+     Group.includes('*') ||
+     Group.includes('(') ||
+     Group.includes(')') ||
+     Group.includes('\\') ||
+     Group.includes('=') ||
+     Group.includes('[') ||
+     Group.includes(']') ||
+     Group.includes(';') ||
+     Group.includes(',') ||
+     Group.includes('.') ||
+     Group.includes('<') ||
+     Group.includes('>') ||
+     Group.includes('?') ||
+     Group.includes('`')
+   ) {
+     errorMessage = 'do not include special characters';
+     setErrorMessage('do not include special characters');
+     submitBtn = true;
+     setSubmitBtn(true);
+   } else if (Group.includes(' ')) {
+     errorMessage = 'No space is allowed';
+     setErrorMessage('No space is allowed');
+     submitBtn = true;
+     setSubmitBtn(true);
+   } else if (Group == '') {
+     errorMessage = 'Details needed';
+     setErrorMessage('Details needed');
+     submitBtn = true;
+     setSubmitBtn(true);
+   } else {
+     errorMessage = '';
+     setErrorMessage('');
+     submitBtn = false;
+     setSubmitBtn(false);
+   }
+ }
+
+ // display table header that is from props
+ const ths = (
+   <tr>
+     <th
+       style={{
+         textAlign: 'left',
+         fontWeight: '700',
+         fontSize: '16px',
+         color: 'black',
+       }}
+     >
+       <div style={{ marginLeft: '10px' }}>List of Users inside this group</div>
+     </th>
+   </tr>
+ );
+
+ // display all the rows that is from props
+ const rows = CurrentSSOGroups.map((items, index) => (
+   <tr key={index}>
+     <td
+       style={{
+         display: 'flex',
+         justifyContent: 'space-between',
+       }}
+     >
+       <Text
+         color="dark"
+         style={{
+           marginLeft: '10px',
+           pointerEvents: false ? 'auto' : 'none',
+         }}
+         component={Link}
+         to="/insidessogroup"
+         variant="link"
+       >
+         {items.name}
+       </Text>
+       <Button
+         variant="default"
+         color="dark"
+         size="md"
+         style={{ marginRight: '15px' }}
+         onClick={() => [deleteGroup(index)]}
+       >
+         Delete
+       </Button>
+     </td>
+   </tr>
+ ));
   return (
     <>
       <AppBase userType="admin">
-        <AdminConsole
-          groupList={data}
-          addObjectLabel="User"
-          deleteObjectLabel="User"
-          tableHeader={['List of Users inside this group']}
-          tableBody={[]}
-          caption="User Management Console"
-          pointerEvents={false}
-        ></AdminConsole>
+
+<div
+      style={{
+        display: 'flex',
+        height: '80vh',
+        justifyContent: 'center',
+      }}
+    >
+      <div className="console">
+        <Modal
+          centered
+          opened={openedAddUserModel}
+          onClose={() => setOpened(false)}
+        >
+          {
+            <div
+              style={{
+                display: 'flex',
+                height: '25vh',
+                flexDirection: 'column',
+              }}
+            >
+              <Textarea
+                placeholder={textField}
+                label={title}
+                size="md"
+                name="password"
+                id="password"
+                required
+                error={errorMessage}
+                onChange={(event) => {
+                  addGroup(event.target.value),
+                    (Group = event.target.value),
+                    validate();
+                }}
+              />
+              <Button
+                variant="default"
+                color="dark"
+                size="md"
+                disabled={submitBtn}
+                onClick={() => add()}
+                style={{
+                  marginLeft: '15px',
+                  alignSelf: 'flex-end',
+                  marginTop: '20px',
+                }}
+              >
+                Submit
+              </Button>
+            </div>
+          }
+        </Modal>
+
+        <ScrollArea
+          style={{
+            height: '90%',
+            width: '100%',
+            marginTop: '1%',
+          }}
+        >
+          <Table captionSide="top" verticalSpacing="sm">
+            <caption
+              style={{
+                textAlign: 'center',
+                fontWeight: '600',
+                fontSize: '24px',
+                color: 'black',
+                marginTop: '5px',
+              }}
+            >
+            User Management Console
+            </caption>
+            <thead style={{}}>{ths}</thead>
+            <tbody>{rows}</tbody>
+          </Table>
+        </ScrollArea>
+
+        <div
+          style={{
+            display: 'flex',
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+          }}
+        >
+          <Button
+            variant="default"
+            color="dark"
+            size="md"
+            onClick={() => setOpened(true)}
+            style={{ marginLeft: '15px' }}
+          >
+            Add User
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
       </AppBase>
     </>
   );
