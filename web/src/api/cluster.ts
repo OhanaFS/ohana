a8ba985d8edba15a7b69f611f26b4e59930674a1
rahulrocksn <70889306+rahulrocksn@users.noreply.github.com>
2022-08-13 13:57:12 +0800

    [wip] Hooks (#66)
    * CLuster hooks
    
    * Maintenance hooks, need to be tested
    
    * Updated clusters with requested changes
    
    * Admin file changed according to cluster
    
    * added requested changes
    
    * Requested changes, Maintenance to be edited(now commented)
    
    * Maintenance hooks
    
    * Maintenance hooks- Requested changes
    
    Co-authored-by: RN <rahulrocksn@pop-os.localdomain>

---
+++ web/src/api/cluster.ts
@@ -0,0 +1,259 @@
+import { APIClient, typedError } from './api';
+import { useMutation, useQuery } from '@tanstack/react-query';
+
+export type Server = {
+  name: string;
+  hostname: string;
+  port: string;
+  status: number;
+  free_space: number;
+  used_space: number;
+};
+
+export type ServerID = {
+  server_id: string;
+};
+
+export type DateTime = {
+  rangeType: number;
+  startDate: string; // need to format 2022-08-05T15:57:05.279Z
+  endDate: string;
+};
+
+export type Logs = {
+  startNum: number;
+  startDate: string; // need to format
+  endDate: string;
+  filter: string;
+};
+
+export type UsedRes = {
+  date: string;
+  value: number;
+};
+
+// Get the number of files from the cluster
+export const useQueryGetnumOfFiles = () =>
+  useQuery(['numOfFiles'], () =>
+    APIClient.get<number>(`/api/v1/cluster/stats/num_of_files`)
+      .then((res) => res.data)
+      .catch(typedError)
+  );
+//working
+
+// Get the number of historical files count from the cluster
+export const useQueryGetnumOfHistoricalFiles = (
+  range: number,
+  startDate: string,
+  endDate: string
+) =>
+  useQuery(['numOfHistoricalFiles', range, startDate, endDate], () =>
+    APIClient.get<UsedRes>(`/api/v1/cluster/stats/num_of_files_historical`, {
+      headers: {
+        rangeType: range,
+        startDate: new Date(startDate).toISOString(),
+        endDate: new Date(endDate).toISOString(),
+      },
+    })
+      .then((res) => res.data)
+      .catch(typedError)
+  );
+
+// Get the storage used without parity and versioning from the cluster
+export const useQueryGetstorageUsed = () =>
+  useQuery(['storageNonReplicaUsed'], () =>
+    APIClient.get<number>(`/api/v1/cluster/stats/non_replica_used`)
+      .then((res) => res.data)
+      .catch(typedError)
+  );
+
+// Get the historical storage used without parity and versioning from the cluster
+export const useQueryGethistoricalStorageUsed = (
+  range: number,
+  startDate: string,
+  endDate: string
+) =>
+  useQuery(['nonReplicaUsedHistorical', range, startDate, endDate], () =>
+    APIClient.get<number>(`/api/v1/cluster/stats/non_replica_used_historical`, {
+      headers: {
+        rangeType: range,
+        // iso format using toisostring
+        startDate: new Date(startDate).toISOString(),
+        endDate: new Date(endDate).toISOString(),
+      },
+    })
+      .then((res) => res.data)
+      .catch(typedError)
+  );
+
+// Get the storage used with parity and versioning from the cluster
+export const useQueryGetstorageUsedWithParity = () =>
+  useQuery(['replica'], () =>
+    APIClient.get<number>(`/api/v1/cluster/stats/replica_used`)
+      .then((res) => res.data)
+      .catch(typedError)
+  );
+
+// Get the historical storage used with parity and versioning from the cluster
+export const useQueryGethistoricalStorageUsedWithParity = (
+  range: number,
+  startDate: string,
+  endDate: string
+) =>
+  useQuery(['replicaHistorical', range, startDate, endDate], () =>
+    APIClient.get<UsedRes>(`/api/v1/cluster/stats/replica_used_historical`, {
+      headers: {
+        rangeType: range,
+        startDate: new Date(startDate).toISOString(),
+        endDate: new Date(endDate).toISOString(),
+      },
+    })
+      .then((res) => res.data)
+      .catch(typedError)
+  );
+
+export type Alerts = {
+  log_entries: LogEntry[];
+  fatal_count: number;
+  error_count: number;
+  warning_count: number;
+};
+
+export type LogEntry = {
+  id: number;
+  log_type: number;
+  server_name: string;
+  message: string;
+  timestamp: string;
+};
+
+// Get all alerts related to the cluster
+export const useQueryGetAlerts = () =>
+  useQuery(['alerts'], () =>
+    APIClient.get<Alerts>(`/api/v1/cluster/stats/alerts`)
+      .then((res) => res.data)
+      .catch(typedError)
+  );
+
+// Clear all alerts related to the cluster
+export const useMutationClearAlerts = () =>
+  useMutation(() =>
+    APIClient.delete<boolean>(`/api/v1/cluster/stats/alerts`)
+      .then((res) => res.data)
+      .catch(typedError)
+  );
+
+// Get specific alerts of the cluster
+export const useQueryGetAlertsID = (Id: number) =>
+  useQuery(['alertsID', Id], () =>
+    APIClient.get<LogEntry>(`/api/v1/cluster/stats/alerts/${Id}`, {
+      headers: {
+        id: Id,
+      },
+    })
+      .then((res) => res.data)
+      .catch(typedError)
+  );
+
+// Clear specific alerts of the cluster
+export const useMutationClearAlertsID = (id: number) =>
+  useMutation((id) =>
+    APIClient.delete<boolean>(`/api/v1/cluster/stats/alerts/${id}`)
+      .then((res) => res.data)
+      .catch(typedError)
+  );
+
+// Return server logs
+export const useQueryGetserverLogs = (
+  start: number,
+  startDate: string,
+  endDate: string,
+  filter: string
+) =>
+  useQuery(['serverLogs', start, startDate, endDate, filter], () =>
+    APIClient.get<LogEntry>(`/api/v1/cluster/stats/logs`, {
+      headers: {
+        startNum: start,
+        startDate: new Date(startDate).toISOString(),
+        endDate: new Date(endDate).toISOString(),
+        filter: filter,
+      },
+    })
+      .then((res) => res.data)
+      .catch(typedError)
+  );
+
+// Clear logs
+export const useMutationClearserverLogs = () =>
+  useMutation(['clearserverLogs'], () =>
+    APIClient.delete<boolean>(`/api/v1/cluster/stats/logs`)
+      .then((res) => res.data)
+      .catch(typedError)
+  );
+
+// Return server logs by id
+export const useQueryGetserverLogsID = (
+  id: number,
+  start: number,
+  startDate: string,
+  endDate: string,
+  filter: string
+) =>
+  useQuery(['serverLogsID', id, start, startDate, endDate, filter], () =>
+    APIClient.get<LogEntry>(`/api/v1/cluster/stats/logs/${id}`, {
+      headers: {
+        startNum: start,
+        startDate: new Date(startDate).toISOString(),
+        endDate: new Date(endDate).toISOString(),
+        filter: filter,
+      },
+    })
+      .then((res) => res.data)
+      .catch(typedError)
+  );
+
+// Return server statuses
+export const useQueryGetserverStatuses = () =>
+  useQuery(['serverStatuses'], () =>
+    APIClient.get<Server>(`/api/v1/cluster/stats/servers`)
+      .then((res) => res.data)
+      .catch(typedError)
+  );
+
+export type ServerStatuses = {
+  name: string;
+  hostname: string;
+  port: string;
+  status: number;
+  free_space: number;
+  used_space: number;
+  load_avg: string;
+  uptime: number;
+  cpu: number;
+  memory_used: number;
+  memory_free: number;
+  network_rx_bytes: number;
+  network_tx_bytes: number;
+  reads: number;
+  writes: number;
+  warnings: number;
+  errors: number;
+  smart_good: boolean;
+  smart_status: string;
+};
+
+// Return server specific statuses
+export const useQueryGetserverStatusesID = (serverName: string) =>
+  useQuery(['serverStatusesID', serverName], () =>
+    APIClient.get<ServerStatuses>(`/api/v1/cluster/stats/servers/${serverName}`)
+      .then((res) => res.data)
+      .catch(typedError)
+  );
+
+// Delete server specific statuses
+export const useMutationDeleteserverStatusesID = (serverName: string) =>
+  useMutation(['deleteserverStatusesID', serverName], () =>
+    APIClient.delete<boolean>(`/api/v1/cluster/stats/servers/${serverName}`)
+      .then((res) => res.data)
+      .catch(typedError)
+  );
---
+++ web/src/api/maintenance.ts
@@ -0,0 +1,297 @@
+import { APIClient, typedError } from './api';
+import { useMutation, useQuery } from '@tanstack/react-query';
+
+export type Record = {
+  Id: 0;
+  date_time_started: string;
+  date_time_ended: string;
+  total_time_taken: string;
+  total_shards_scanned: number;
+  total_files_scanned: number;
+  tasks: {
+    job_type: string;
+    id: number;
+    status: number;
+  }[];
+  progress: number;
+  status_msg: string;
+  status: number;
+};
+
+// Get all the records
+export const useQueryGetMaintenanceRecords = (
+  startNum: number,
+  startDate: string,
+  endDate: string,
+  filter: string
+) =>
+  useQuery(['mainRecords'], () =>
+    APIClient.get<Record>(`/api/v1/maintenance/all`, {
+      headers: {
+        startNum: startNum,
+        startDate: new Date(startDate).toISOString(),
+        endDate: new Date(endDate).toISOString(),
+        filter: filter,
+      },
+    })
+      .then((res) => res.data)
+      .catch(typedError)
+  );
+
+// Get the records based on the ID
+export const useQueryGetMaintenanceRecordsID = (id: number) => {
+  return useQuery(['mainRecordsID', id], () =>
+    APIClient.get<Record>(`/api/v1/maintenance/job/${id}`)
+      .then((res) => res.data)
+      .catch(typedError)
+  );
+};
+
+// Delete the records based on the ID
+const useMutateDeleteMainRecordsID = () => {
+  return useMutation((id: number) =>
+    APIClient.delete<Record>(`/api/v1/maintenance/job/${id}`)
+      .then((res) => res.data)
+      .catch(typedError)
+  );
+};
+
+// Create a job based on the ID
+const useMutateCreateMainRecordsID = () => {
+  return useMutation((id: number) =>
+    APIClient.patch<boolean>(`/api/v1/maintenance/job/${id}`)
+      .then((res) => res.data)
+      .catch(typedError)
+  );
+};
+
+export type MaintenanceRecordCheck = {
+  full_shards_check: boolean;
+  quick_Shards_check: boolean;
+  missing_shards_check: boolean;
+  orphaned_shards_check: boolean;
+  orphaned_files_check: boolean;
+  permission_check: boolean;
+  delete_fragments: boolean;
+};
+
+// Start a job based on the ID
+const useMutateStartMainRecordsID = () => {
+  return useMutation((params: MaintenanceRecordCheck) =>
+    APIClient.post<Record>(`/api/v1/maintenance/start`, {
+      headers: {
+        full_shards_check: String(params.full_shards_check),
+        quick_Shards_check: String(params.quick_Shards_check),
+        missing_shards_check: String(params.missing_shards_check),
+        orphaned_shards_check: String(params.orphaned_shards_check),
+        orphaned_files_check: String(params.orphaned_files_check),
+        permission_check: String(params.permission_check),
+        delete_fragments: String(params.delete_fragments),
+      },
+    })
+      .then((res) => res.data)
+      .catch(typedError)
+  );
+};
+
+export type ShardsResults = {
+  file_id: string;
+  file_name: string;
+  data_id: string;
+  fragment_id: string;
+  server_name: string;
+  status: number;
+};
+
+// Get full shards job results
+const useQueryGetFullShardsResults = (id: number) =>
+  useQuery(['fullShardsResults', id], () =>
+    APIClient.get<ShardsResults>(`/api/v1/maintenance/jon/${id}/full_shards`)
+      .then((res) => res.data)
+      .catch(typedError)
+  );
+
+// Fix full shards job
+const useMutateFixFullShards = (id: number) => {
+  return useMutation((id) =>
+    APIClient.get<ShardsResults>(`/api/v1/maintenance/jon/${id}/full_shards`)
+      .then((res) => res.data)
+      .catch(typedError)
+  );
+};
+
+// Get quick shards job results
+const useQueryGetQuickShardsResults = (id: number) =>
+  useQuery(['quickShardsResults', id], () =>
+    APIClient.get<ShardsResults>(`/api/v1/maintenance/jon/${id}/quick_shards`)
+      .then((res) => res.data)
+      .catch(typedError)
+  );
+
+export type QuickShardsJob = {
+  file_id: string;
+  fragment_id: string;
+  password: string;
+  action: number;
+};
+
+// Fix quick shards job
+const useMutatefixQuickShards = () => {
+  return useMutation((id: number) =>
+    APIClient.post<QuickShardsJob>(`/api/v1/maintenance/jon/${id}/quick_shards`)
+      .then((res) => res.data)
+      .catch(typedError)
+  );
+};
+
+// Get missing shards job results
+const useQueryGetMissingShardsResults = (id: number) =>
+  useQuery(['missingShardsResults', id], () =>
+    APIClient.get<ShardsResults>(`/api/v1/maintenance/jon/${id}/missing_shards`)
+      .then((res) => res.data)
+      .catch(typedError)
+  );
+
+// Fix missing shards job
+const useMutateFixMissingShards = () => {
+  return useMutation((id: number) =>
+    APIClient.post<QuickShardsJob>(
+      `/api/v1/maintenance/jon/${id}/missing_shards`
+    )
+      .then((res) => res.data)
+      .catch(typedError)
+  );
+};
+
+// Get orphaned shards job results
+const useQueryGetOrphanedShardsResults = (id: number) =>
+  useQuery(['orphanedShardsResults', id], () =>
+    APIClient.get<ShardsResults>(
+      `/api/v1/maintenance/jon/${id}/orphaned_shards`
+    )
+      .then((res) => res.data)
+      .catch(typedError)
+  );
+
+// Fix orphaned shards job
+const useMutateFixOrphanedShards = () => {
+  return useMutation((id: number) =>
+    APIClient.post<QuickShardsJob>(
+      `/api/v1/maintenance/jon/${id}/orphaned_shards`
+    )
+      .then((res) => res.data)
+      .catch(typedError)
+  );
+};
+
+// Get orphaned files job results
+const useQueryGetOrphanedFilesResults = (id: number) =>
+  useQuery(['orphanedFilesResults', id], () =>
+    APIClient.get<ShardsResults>(`/api/v1/maintenance/jon/${id}/orphaned_files`)
+      .then((res) => res.data)
+      .catch(typedError)
+  );
+
+// Fix orphaned files job
+const useMutateFixOrphanedFiles = () => {
+  return useMutation((id: number) =>
+    APIClient.post<QuickShardsJob>(
+      `/api/v1/maintenance/jon/${id}/orphaned_files`
+    )
+      .then((res) => res.data)
+      .catch(typedError)
+  );
+};
+
+// Get permission check on jobs
+const useQueryGetPermissionCheckResults = (id: number) =>
+  useQuery(['permissionCheckResults', id], () =>
+    APIClient.get<ShardsResults>(
+      `/api/v1/maintenance/jon/${id}/permission_check`
+    )
+      .then((res) => res.data)
+      .catch(typedError)
+  );
+
+// Fix permission check job
+const useMutateFixPermissionCheck = () => {
+  return useMutation((id: number) =>
+    APIClient.post<QuickShardsJob>(
+      `/api/v1/maintenance/jon/${id}/permission_check`
+    )
+      .then((res) => res.data)
+      .catch(typedError)
+  );
+};
+
+// Get backup keys
+const useQueryGetBackupKeys = () =>
+  useQuery(['backupKeys'], () =>
+    APIClient.get<boolean>(`/api/v1/maintenance/backup_keys`)
+      .then((res) => res.data)
+      .catch(typedError)
+  );
+
+// needs changes
+// Send backup keys
+const useMutateSendBackupKeys = () => {
+  return useMutation((id: number) =>
+    APIClient.put<boolean>(`/api/v1/maintenance/backup_keys`)
+      .then((res) => res.data)
+      .catch(typedError)
+  );
+};
+
+// needs changes
+const useMutatePostFileKey = () => {
+  return useMutation((fileID: number) =>
+    APIClient.post<boolean>(`/api/v1/maintenance/KEY/${fileID}`)
+      .then((res) => res.data)
+      .catch(typedError)
+  );
+};
+
+// Deleting file key
+const useMutateDeleteFileKey = () => {
+  return useMutation((fileID: number) =>
+    APIClient.delete<boolean>(`/api/v1/maintenance/KEY/${fileID}`)
+      .then((res) => res.data)
+      .catch(typedError)
+  );
+};
+
+// needs changes
+const useMutatePostFolderKey = () => {
+  return useMutation((folderID: number) =>
+    APIClient.post<boolean>(`/api/v1/maintenance/key/${folderID}`)
+      .then((res) => res.data)
+      .catch(typedError)
+  );
+};
+
+// Deleting folder key
+const useMutateDeleteFolderKey = () => {
+  return useMutation((folderID: number) =>
+    APIClient.delete<any>(`/api/v1/maintenance/key/${folderID}`)
+      .then((res) => res.data)
+      .catch(typedError)
+  );
+};
+
+// Deletion master key
+const useMutationDeleteMasterKey = () => {
+  return useMutation((serverID: number) =>
+    APIClient.delete<boolean>(`/api/v1/maintenance/key/${serverID}`)
+      .then((res) => res.data)
+      .catch(typedError)
+  );
+};
+
+// Request master key
+export const useMutationRequestMasterKey = () => {
+  return useMutation((serverID: number) =>
+    APIClient.put<boolean>(`/api/v1/maintenance/key/${serverID}`)
+      .then((res) => res.data)
+      .catch(typedError)
+  );
+};
