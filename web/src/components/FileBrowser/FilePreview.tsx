8dbe3224654807232816b661feb121bc30a18b68
Hizkia Felix Winata <felix@hizkifw.me>
2022-08-14 16:55:41 +0800

    chonky: preview text files

--- web/src/components/FileBrowser/FilePreview.tsx
+++ web/src/components/FileBrowser/FilePreview.tsx
@@ -1,5 +1,25 @@
-import { Image, Text } from '@mantine/core';
+import { Code, Image, Loader, Text } from '@mantine/core';
+import { useQuery } from '@tanstack/react-query';
+import { APIClient } from '../../api/api';
 import { getFileDownloadURL, useQueryFileMetadata } from '../../api/file';
+
+type TextFilePreviewProps = {
+  url: string;
+};
+
+const TextFilePreview = ({ url }: TextFilePreviewProps) => {
+  const qFile = useQuery(['file', url], () =>
+    APIClient.get<string>(url).then((res) => res.data)
+  );
+
+  if (qFile.isLoading) return <Loader />;
+
+  return (
+    <Code className="w-full overflow-x-scroll" px="md">
+      <pre>{qFile.data || ''}</pre>
+    </Code>
+  );
+};
 
 export type FilePreviewProps = {
   fileId: string;
@@ -15,6 +35,8 @@ const FilePreview = ({ fileId }: FilePreviewProps) => {
     <video className="w-full" controls playsInline src={downloadUrl}></video>
   ) : qFile.data?.mime_type.startsWith('audio/') ? (
     <audio className="w-full" controls playsInline src={downloadUrl}></audio>
+  ) : qFile.data?.mime_type.startsWith('text/') ? (
+    <TextFilePreview url={downloadUrl} />
   ) : (
     <Text>{qFile.data?.file_name || ''}</Text>
   );
